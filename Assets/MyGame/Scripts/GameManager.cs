a90fe4c0343abcf2529a69d2d25ea8f0bfdaa60a
bgreis <benedikt.greisberger04@htl-salzburg.ac.at>
2023-01-27 10:31:40 +0100

    End Screens

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,31 +1,72 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject fischrot;
+    public GameObject fischgrün;
+    public GameObject fischlila;
     public GameObject fischorange;
-    public GameObject fischpermagelb;
-    public GameObject fischgelb;
+    public GameObject fischrot;
     public GameObject boot;
+
+    private int Intgrün;
+    private int Intlila;
+    private int Intorange;
+    private int Introt;
+    private int Intboot; 
 
     private int Würfel;
 
-    void Start()
+    public void Start()
     {
         Würfel = 0;
+        Intgrün = 0;
+        Intlila = 0;
+        Intorange = 0;
+        Introt = 0;
+        Intboot = 0;
     }
 
-    void Update()
+    public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Würfeln();
         }
+        if (Intgrün == 8)
+        {
+            Debug.Log("Ende Fische");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        else if (Intlila == 8)
+        {
+            Debug.Log("Ende Fische");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        else if (Intorange == 8)
+        {
+            Debug.Log("Ende Fische");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        else if (Introt == 8)
+        {
+            Debug.Log("Ende Fische");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        else if (Intboot == 8)
+        {
+            Debug.Log("Ende Boot");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
     }
 
-    void Würfeln()
+    public void Würfeln()
     {
-        Würfel = Random.Range(1, 7); // Würfelwert zwischen 1 und 6
+        Würfel = Random.Range(1, 7); 
 
         Debug.Log("Würfel: " + Würfel);
 
@@ -33,27 +74,38 @@ public class GameManager : MonoBehaviour
         {
             if (Würfel == 1)
             {
-                fischrot.transform.position += new Vector3(1, 0, 0);
+                fischgrün.transform.position += new Vector3(1, 0, 0);
+                Intgrün++;
+                Debug.Log(Intgrün);
             }
             else if (Würfel == 2)
             {
-                fischorange.transform.position += new Vector3(1, 0, 0);
+                fischlila.transform.position += new Vector3(1, 0, 0);
+                Intlila++;
+                Debug.Log(Intlila);
             }
             else if (Würfel == 3)
             {
-                fischpermagelb.transform.position += new Vector3(1, 0, 0);
+                fischorange.transform.position += new Vector3(1, 0, 0);
+                Intorange++;
+                Debug.Log(Intorange);
             }
             else if (Würfel == 4)
             {
-                fischgelb.transform.position += new Vector3(1, 0, 0);
+                fischrot.transform.position += new Vector3(1, 0, 0);
+                Introt++;
+                Debug.Log(Introt);
             }
         }
         else if (Würfel == 5 || Würfel == 6)
         {
             boot.transform.position += new Vector3(1, 0, 0);
+            Intboot++;
+            Debug.Log(Intboot);
         }
     }
 
+    
 
 }
 
